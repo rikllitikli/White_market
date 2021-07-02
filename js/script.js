463ac3511ac5830b75a19a58571529d5f8534402
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-07-02 16:27:00 +0300

    add tradingview

--- js/script.js
+++ js/script.js
@@ -18,3 +18,48 @@ $(document).ready(function(){
     });
 });
 
+$('.btn_q').on('click', function(){
+    $('.text_q').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_w').on('click', function(){
+    $('.text_w').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_e').on('click', function(){
+    $('.text_e').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_r').on('click', function(){
+    $('.text_r').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_t').on('click', function(){
+    $('.text_t').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_y').on('click', function(){
+    $('.text_y').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_u').on('click', function(){
+    $('.text_u').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_i').on('click', function(){
+    $('.text_i').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});
+$('.btn_o').on('click', function(){
+    $('.text_o').fadeToggle('slow');
+    //or
+    //$(this).next().fadeToggle('slow');
+});

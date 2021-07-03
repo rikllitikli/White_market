af8ebc8e04fc257d16f3c2b516d6420d50b53cb3
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-07-03 12:39:57 +0300

    add tradingview

--- js/script.js
+++ js/script.js
@@ -63,3 +63,20 @@ $('.btn_o').on('click', function(){
     //or
     //$(this).next().fadeToggle('slow');
 });
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
